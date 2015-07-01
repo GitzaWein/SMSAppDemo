6708280cf348a48d4c54431c97b9b39512af4ff3
GitzaWein <gitzaweinstock@gmail.com>
2015-07-01 14:34:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/student/smsappdemo/SMSReceiver.java
@@ -0,0 +1,40 @@
+package com.example.student.smsappdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by student on 7/1/2015.
+ */
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+        //get extras from Intent
+        Bundle extras = intent.getExtras();
+        SmsMessage[] messages = null;
+        String senderNumber = "";
+        String msgBody = "";
+        if(extras != null){
+            //retrieve the SMS message chunks
+            Object[] pdus = (Object[]) extras.get("pdus");
+            messages = new SmsMessage[pdus.length];
+            for(int i = 0; i < messages.length; i++){
+                messages[1] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                if(i == 0){
+                    //get the sender number
+                    senderNumber = messages[i].getOriginatingAddress();
+                }
+                //get message body
+                msgBody += messages[i].getMessageBody();
+            }
+            //SmsManager smsManager = SmsManager.getDefault();
+           // smsManager.sendTextMessage(senderNumber,null,"This is an auto reply sms.",null,null);
+            Toast.makeText(context,"SMS received from: " + senderNumber + "\nSMS content: " + msgBody, Toast.LENGTH_LONG).show();
+        }
+    }
+}
